4bcd653280178a882d430bd5f50e2195053e3257
TJ Saunders <tj@castaglia.org>
2015-06-26 20:57:02 -0700

    Support a DisplayCode option, for telling clients to echo the OTP code entered by users.

--- mod_auth_otp.c
+++ mod_auth_otp.c
@@ -36,6 +36,7 @@
 /* mod_auth_otp option flags */
 #define AUTH_OTP_OPT_STANDARD_RESPONSE		0x001
 #define AUTH_OTP_OPT_REQUIRE_TABLE_ENTRY	0x002
+#define AUTH_OTP_OPT_DISPLAY_CODE		0x004
 
 /* From src/response.c */
 extern pr_response_t *resp_list;
@@ -102,9 +103,11 @@ static int auth_otp_kbdint_authenticate(sftp_kbdint_driver_t *driver,
 
   challenge = pcalloc(driver->driver_pool, sizeof(sftp_kbdint_challenge_t));
   challenge->challenge = pstrdup(driver->driver_pool, "Verification code: ");
-
-  /* This MUST be set to FALSE, in order to receive the code from the client. */
   challenge->display_response = FALSE;
+
+  if (auth_otp_opts & AUTH_OTP_OPT_DISPLAY_CODE) {
+    challenge->display_response = TRUE;
+  }
 
   if (sftp_kbdint_send_challenge(NULL, NULL, 1, challenge) < 0) {
     pr_trace_msg(trace_channel, 3,
@@ -606,6 +609,9 @@ MODRET set_authotpoptions(cmd_rec *cmd) {
 
     } else if (strcmp(cmd->argv[i], "RequireTableEntry") == 0) {
       opts |= AUTH_OTP_OPT_REQUIRE_TABLE_ENTRY;
+
+    } else if (strcmp(cmd->argv[i], "DisplayCode") == 0) {
+      opts |= AUTH_OTP_OPT_DISPLAY_CODE;
 
     } else {
       CONF_ERROR(cmd, pstrcat(cmd->tmp_pool, ": unknown AuthOTPOption: '",
